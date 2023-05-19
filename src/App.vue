e9c8fc14323755f030299d28f63eb9e87ca3de12
forcejie <2963301696@qq.com>
2023-05-19 17:21:16 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="home">
+    <el-button>Default</el-button>
+    <el-button type="primary">Primary</el-button>
+    <el-button type="success">Success</el-button>
+    <el-button type="info">Info</el-button>
+    <el-button type="warning">Warning</el-button>
+  </div>
+</template>
+
+<script setup></script>
+
+<style lang="less" scoped></style>
